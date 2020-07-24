a5edd400f7c0816c00b6a7d91cfee56783b8e596
Shivankshikh <shivankshikh@gmail.com>
2020-07-25 00:02:18 +0530

    home controller

---
+++ emscrudUsingSpringDataRest/src/main/java/com/shivankshi/emscrud/rest/HomeController.java
@@ -0,0 +1,24 @@
+package com.shivankshi.emscrud.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	Logger logger=LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		logger.info("welcome in the EMS");
+		logger.debug("debug level log");
+	    logger.info("info level log");
+	    logger.error("error level log");
+	    logger.trace("In home Controller");
+		return "Welcome to the Employee Management System Web Application";
+	}
+
+}
